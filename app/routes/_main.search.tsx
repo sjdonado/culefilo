45a8d60c6ba866cc4115db54459de63cc71e3679
Juan Rodriguez <sjdonado@icloud.com>
2024-04-06 14:53:55 +0200

    feat: KV setup - search endpoint

--- app/routes/_main.search.tsx
+++ app/routes/_main.search.tsx
@@ -3,7 +3,8 @@ import { MagnifyingGlassIcon, MapPinIcon } from '@heroicons/react/24/outline';
 import { ValidatedForm, validationError } from 'remix-validated-form';
 import { withZod } from '@remix-validated-form/with-zod';
 
-import type { ActionFunctionArgs } from '@remix-run/node';
+import type { LoaderFunctionArgs, ActionFunctionArgs } from '@remix-run/cloudflare';
+import { redirect, useLoaderData } from '@remix-run/react';
 
 import { SearchSchema } from '~/schemas/search';
 
@@ -14,7 +15,7 @@ import SubmitButton from '~/components/SubmitButton';
 
 const validator = withZod(SearchSchema);
 
-export const action = async ({ request }: ActionFunctionArgs) => {
+export const action = async ({ request, context }: ActionFunctionArgs) => {
   const fieldValues = await validator.validate(await request.formData());
 
   if (fieldValues.error) {
@@ -24,12 +25,40 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const { favoriteMealName, zipCode } = fieldValues.data;
 
   const location = await getLocationDataFromZipCode(zipCode);
+
   console.log({ favoriteMealName, zipCode, location });
 
-  return null;
+  const key = crypto.randomUUID();
+
+  await context.cloudflare.env.CULEFILO_KV.put(
+    key,
+    JSON.stringify({
+      favoriteMealName,
+      zipCode,
+      location,
+    })
+  );
+
+  return redirect(`/search?id=${key}`);
+};
+
+export const loader = async ({ request, context }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+
+  const key = url.searchParams.get('id');
+
+  const search = context.cloudflare.env.CULEFILO_KV.get(key as string, {
+    type: 'json',
+  });
+
+  return { search };
 };
 
 export default function SearchPage() {
+  const { search } = useLoaderData<typeof loader>();
+
+  console.log('client', search);
+
   return (
     <ValidatedForm validator={validator} method="post">
       <div className="border-base-custom rounded-lg border bg-base-200/30 p-4 md:p-6">
