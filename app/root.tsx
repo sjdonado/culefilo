d8d716be029a2a50250b48a5f4592311d4a0fe3d
Juan Rodriguez <sjdonado@icloud.com>
2024-04-08 15:09:11 +0200

    chore: udpate metadata

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,26 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Search for your favorite meal near you, quickly, enhanced by AI and for free."
+        />
+
+        <meta property="og:title" content="Cule filo" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://culefilo.pages.dev" />
+        <meta property="og:site_name" content="Cule filo" />
+        <meta
+          property="og:description"
+          content="Search for your favorite meal near you, quickly, enhanced by AI and for free."
+        />
+
+        <title>Cule filo</title>
+
         <Meta />
         <Links />
       </head>
