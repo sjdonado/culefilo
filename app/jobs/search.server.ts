fb2038e3e91f4cbfe194925e01a7eff40f57fbe1
Jose Hernandez <gjhernandez@uninorte.edu.co>
2024-04-10 01:32:05 +0200

    feature: reduce Google API results to 3

--- app/jobs/search.server.ts
+++ app/jobs/search.server.ts
@@ -153,7 +153,7 @@ export async function startOrCheckSearchJob(context: AppLoadContext, key: string
           }
         }
 
-        if (allPlaces.size > 3) {
+        if (allPlaces.size >= 3) {
           sendEvent(
             'Summarizing results...',
             increaseProgress(progress < 0.4 ? 0.4 - progress : 0)
